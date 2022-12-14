d60a3d674451d75ae0045c46c52f89c2bc334f2c
Joyce Pereira Oliveira <98670029+erickbarauna@users.noreply.github.com>
2022-12-14 07:19:19 -0300

    aula-06
    inserindo css

---
+++ aulas/aula-06/inserindo-css/src/components/Frase.js
@@ -0,0 +1,13 @@
+import styles from "./Frase.css"
+
+function Frase() {
+    return (
+        <div className="fraseContainer">
+            <a href="https://www.youtube.com/watch?v=20hlPRPVMoU&list=PLnDvRpP8BneyVA0SZ2okm-QBojomniQVO&index=6" target="_blank">
+                Este link é um componente!
+            </a>
+        </div>
+    )
+}
+
+export default Frase
