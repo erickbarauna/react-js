3230c235dba096f11a95bed5e90dfbf65b5ac467
Joyce Pereira Oliveira <98670029+erickbarauna@users.noreply.github.com>
2022-12-19 07:19:46 -0300

    aula-12
    renderização condicional

---
+++ aulas/aula-12/renderizacao-condicional/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Condicional from './components/Condicional';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Renderização Condicional</h1>
+      <Condicional />
+    </div>
+  );
+}
+
+export default App;
