f66b492c381e5e00cb7123b04b643d826ca43db3
Joyce Pereira Oliveira <98670029+erickbarauna@users.noreply.github.com>
2023-01-12 07:55:49 -0300

    Criando Rotas

--- aulas/projeto/costs/src/App.js
+++ aulas/projeto/costs/src/App.js
@@ -1,9 +1,25 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
 
 function App() {
   return (
-    <div className="App">
-      <p>costs</p>
-    </div>
+    <Router>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/company'>Empresa</Link></li>
+        <li><Link to='/contact'>Contato</Link></li>
+        <li><Link to='/newproject'>Novo Projeto</Link></li>
+      </ul>
+      <Routes>
+        <Route exact path="/" element={<Home />}/>
+        <Route exact path="/company" element={<Company />}/>
+        <Route exact path="/contact" element={<Contact />}/>
+        <Route exact path="/newproject" element={<NewProject />}/>
+      </Routes>
+    </Router>
   );
 }
 
