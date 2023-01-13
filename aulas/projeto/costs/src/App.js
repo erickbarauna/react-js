1c9da360b52cdbf17245e49f23c979381495ca32
Joyce Pereira Oliveira <98670029+erickbarauna@users.noreply.github.com>
2023-01-13 06:53:55 -0300

    aula18 finalizada - estruturando o projeto

--- aulas/projeto/costs/src/App.js
+++ aulas/projeto/costs/src/App.js
@@ -3,22 +3,26 @@ import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
+import Container from './components/layout/Container';
 
 function App() {
   return (
     <Router>
-      <ul>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/company'>Empresa</Link></li>
-        <li><Link to='/contact'>Contato</Link></li>
-        <li><Link to='/newproject'>Novo Projeto</Link></li>
-      </ul>
-      <Routes>
-        <Route exact path="/" element={<Home />}/>
-        <Route exact path="/company" element={<Company />}/>
-        <Route exact path="/contact" element={<Contact />}/>
-        <Route exact path="/newproject" element={<NewProject />}/>
-      </Routes>
+      <div>
+        <Link to='/'>Home</Link>
+        <Link to='/company'>Empresa</Link>
+        <Link to='/contact'>Contato</Link>
+        <Link to='/newproject'>Novo Projeto</Link>
+      </div>
+      <Container customClass='min-height'>
+        <Routes>
+          <Route exact path="/" element={<Home />}/>
+          <Route exact path="/company" element={<Company />}/>
+          <Route exact path="/contact" element={<Contact />}/>
+          <Route exact path="/newproject" element={<NewProject />}/>
+        </Routes>
+      </Container>
+      <p>Footer</p>
     </Router>
   );
 }
