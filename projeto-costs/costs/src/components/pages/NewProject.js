a69d8a4b587638d8de8cda79ce6cee625bb335b0
Joyce Pereira Oliveira <98670029+erickbarauna@users.noreply.github.com>
2023-01-20 07:32:08 -0300

    projeto costs - componentização do formulário

--- projeto-costs/costs/src/components/pages/NewProject.js
+++ projeto-costs/costs/src/components/pages/NewProject.js
@@ -6,8 +6,7 @@ function NewProject() {
         <div className={styles.newproject_container}>
             <h1>Criar Projeto</h1>
             <p>Crie seu projeto para depois adicionar os serviços</p>
-            <p>Formulário</p>
-            <ProjectForm />
+            <ProjectForm btnText="Criar Projeto"/>
         </div>
     )
 }
